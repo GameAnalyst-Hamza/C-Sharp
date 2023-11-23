0901b174bfc0fbc7471f8acf7ab221c494bba34e
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 20:16:44 +0500

    Reference Code for Encapsulation

---
+++ ConsoleApp/ConsoleApp/Bank.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    // First method to implemet Encapsulation
+    public class Bank1
+    {
+        private double balance;
+
+        //Accessor
+        public double getBalance()
+        {
+            return balance;
+        }
+
+        //Mutator
+        public void setBalance(double balance)
+        {
+            if(balance <= 0)
+            {
+                Console.WriteLine("Balance is less than Zero");
+            }
+            else
+            {
+                this.balance = balance;
+            }
+        }
+    }
+
+    // Another method to implemet Encapsulation
+    public class Bank2
+    {
+        private double balance;
+        public double Balance
+        {
+            get
+            {
+
+                return balance;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Balance is less than Zero");
+                }
+                else
+                {
+                    this.balance = value;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double accountBalance1 = 0;
+            double accountBalance2 = 1000;
+
+            Bank1 bankObj = new Bank1();
+            bankObj.setBalance(accountBalance1);
+            Console.WriteLine(bankObj.getBalance());
+
+            Bank2 bankObj2 = new Bank2();
+            bankObj2.Balance = accountBalance2;
+            Console.WriteLine(bankObj2.Balance);
+
+            Console.ReadKey();
+        }
+    }
+}
