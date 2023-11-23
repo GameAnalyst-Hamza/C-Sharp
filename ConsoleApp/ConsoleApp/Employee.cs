12d5ae69ee4a3d18fb34e1384a745a52ea5556b2
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 20:00:35 +0500

    Reference Code for Classes and Objects

---
+++ ConsoleApp/ConsoleApp/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public void CheckIn()
+        {
+            Console.WriteLine(Name + " has checked in at " + DateTime.Now);
+        }
+
+        public void CheckOut()
+        {
+            Console.WriteLine(Name + " has checked out at " + DateTime.Now);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Initialization
+            Employee employeeObj = new Employee();
+
+            //Setting Properties
+            employeeObj.Id = 1;
+            employeeObj.Name = "Hamza";
+            employeeObj.Department = "Game QA";
+
+            //Actions
+            employeeObj.CheckIn();
+            employeeObj.CheckOut();
+
+            Console.ReadKey();
+        }
+    }
+}
