a491912f6f29efeaef2bec0527be720048e77ed7
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 20:14:32 +0500

    Reference Code for Constants

---
+++ ConsoleApp/ConsoleApp/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Hamza";
+            myName = "Malik";
+
+            // Following line gives error as we are trying to change a constant variable value
+            // const string myName = "Eren";
+            // myName = "Yeager";
+
+            //Calculate area of Circle
+            // pi * r^2
+            const double pi = 3.142;
+            Console.WriteLine("Enter Radius of Circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = pi * radius * radius;
+            Console.WriteLine("Area of Circle is: " + area);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/ReadOnly.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnly
+{
+    internal class Program
+    {
+        readonly double radius = 0;
+        //Constructor
+        public Program(double r)
+        {
+            radius = r;
+        }
+
+        static void Main(string[] args)
+        {
+            //Calculate area of Circle
+            // pi * r^2
+            const double pi = 3.142;
+
+            var programObj = new Program(5);
+            double area = pi * Math.Pow(programObj.radius, 2);
+            Console.WriteLine("Area of circle is: " + area);
+
+            Console.ReadLine();
+        }
+    }
+}
