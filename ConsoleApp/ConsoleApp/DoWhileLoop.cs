c2eaad55ad3a2998e209067eca5a882d0a6727a8
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 20:21:07 +0500

    Reference Code for Loops

---
+++ ConsoleApp/ConsoleApp/DoWhileLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Hello World " + i);
+                i++;
+            } while (i < 0); //It will execute the block atleast even though condition is getting failed
+
+
+            Console.ReadLine();
+        }
+    }
+}
