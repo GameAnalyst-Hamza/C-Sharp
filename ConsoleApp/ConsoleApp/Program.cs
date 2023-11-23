323c03d608dd3e6558197e504bd403bc20d5d964
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 20:20:20 +0500

    Reference Code for Inheritance

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Initialization
+            SimpleCalculator calculatorObj = new SimpleCalculator();
+            double result = calculatorObj.Add(10, 2);
+            Console.WriteLine("Result is " + result);
+
+            //Initializing Object from Inheritance Class
+            ScientificCalculator calculatorObj0 = new ScientificCalculator();
+            double result0 = calculatorObj0.Add(10, 2);
+            Console.WriteLine("Result0 is " + result0);
+
+            double result1 = calculatorObj0.Sin(30);
+            Console.WriteLine("Result1 is " + result1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/ScientificCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //Inheriting SimpleCalculator Class
+    internal class ScientificCalculator : SimpleCalculator
+    {
+        public double Sin(double radius)
+        {
+            return Math.Sin(radius);
+        }
+    }
+}
