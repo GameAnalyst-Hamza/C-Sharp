52a293e05fb0ffab9dc49ca68dda83a3ef497ac1
GameAnalyst-Hamza <dev.hamzasultan@gmail.com>
2023-11-23 19:59:20 +0500

    Reference Code for Arrays

---
+++ ConsoleApp/ConsoleApp/Arrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] intArr; //Array Declaration
+            //int[] intArr = new int[10]; //Array Initialization
+
+            int[] intArr = new int[10];
+
+            int defaultNum = 0;
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                intArr[i] = defaultNum;
+                defaultNum += 2;
+            }
+
+            foreach (var x in intArr)
+            {
+                Console.WriteLine("Array item " + x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
